8e1e05c881b51ef454ec84921a6c5932c2f76aad
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2023-10-27 00:34:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Exemplo de script.js
+document.addEventListener("DOMContentLoaded", function () {
+    // Este código será executado quando a página for carregada
+    const myButton = document.querySelector("#myButton");
+  
+    myButton.addEventListener("click", function () {
+      alert("Botão clicado!");
+    });
+  });
+  
