0b8fad23a173bf39160c166fe84c11da3a982c66
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2024-01-30 18:44:00 +0000

    1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-    res.send.sendFile(__dirname + "/html/index.html")
+    res.sendFile(__dirname + "/html/index.html");
 });
 
 app.get("/Login", function(req, res){
